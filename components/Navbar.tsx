519c253e5974be6c9594fd2009b11400ab3f1a79
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-10-01 12:48:24 -0400

    Add basic landing page

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NavBar() {
+
+  return (
+    <nav className="nav">
+      <div className="nav__left">
+        <h1>StudySync</h1>
+      </div>
+    </nav>
+  );
+}
