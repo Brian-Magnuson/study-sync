8e6f58b327154a74a64a2184013647084395a95c
syronc <syronc@ufl.edu>
2023-10-10 17:13:16 -0400

    added suggested and your topics to home, moved topics component to main components folder

--- components/topic.tsx
+++ components/topic.tsx
@@ -1,26 +1,30 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react';
-import s from '../home.module.css';
-
-
-
+import s from '../app/home/home.module.css';
 interface FormatDateProps {
     name: string
     logo: string
+    page: string
   }
 
-export default function Topic({ name, logo }: FormatDateProps): JSX.Element {
+export default function Topic({ name, logo, page }: FormatDateProps): JSX.Element {
+  const { push } = useRouter();
+  function handleClick() {
+    push('topics/'+page)
+    
+} 
     return (
+      <div
+      onClick={handleClick}>
         <main className={s.topic}>
-            
             <div className={s.column}>
             <img src={logo} alt="Image" />
             </div>
             <div className={s.column}>
             <h2>{name}</h2>
             </div>
-
-            
-
         </main>
+        </div>
       );
   }
