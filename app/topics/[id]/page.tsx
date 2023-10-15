1fef899332a07a00c33b047d729e4db0badbb5c4
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-10-15 16:11:50 -0400

    Add topic page for messaging

--- app/topics/[id]/page.tsx
+++ app/topics/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Sidebar from '@/components/Sidebar';
 import React from 'react';
 import s from './TopicId.module.css';
@@ -9,10 +11,43 @@ type TopicIdPageProps = {
 }
 export default function TopicIdPage(props: TopicIdPageProps) {
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+
   return (
     <main className={s.topic_page}>
       <Sidebar />
-      This is the topic page for topic {props.params.id}
+      <section>
+        <h2>Operating Systems</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label
+            htmlFor="post__file_input"
+            className={s.post__file_input_label}
+          >
+            <span className="material-symbols-outlined">
+              attach_file
+            </span>
+          </label>
+          <input
+            type="file"
+            id="post__file_input"
+          />
+          <input
+            type="text"
+            placeholder="Message"
+          />
+          <button
+            type="submit"
+            className={s.post__submit_btn}
+          >
+            <span className="material-symbols-outlined">
+              send
+            </span>
+          </button>
+        </form>
+      </section>
     </main>
   );
 }
