0d68f73c8c9086f9667e64af855213becb3f7cf5
CJ <321fireworks@gmail.com>
2023-11-15 17:31:33 -0500

    Add "join topic: " popup to suggested topic

--- components/TopicCard.tsx
+++ components/TopicCard.tsx
@@ -1,28 +1,24 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 type FormatDateProps = {
   name: string
   logo: string
   page: string
+  onClick: () => void;
 }
 export default function TopicCard(props: FormatDateProps): JSX.Element {
+  const handleClick = () => {
+    props.onClick();
+  };
 
   return (
-    <Link
-      href={`/discussion/${props.page}`}
-    >
-      <div className="topic">
-        <Image
-          src={props.logo}
-          alt=""
-          width={64}
-          height={64}
-        />
-        <div className="topic__text">
-          <h4>{props.name}</h4>
+    <div className="topic" onClick={handleClick}>
+        <div>
+          <Image src={props.logo} alt="" width={64} height={64} />
+          <div className="topic__text">
+            <h4>{props.name}</h4>
+          </div>
         </div>
-      </div>
-    </Link>
+    </div>
   );
 }
